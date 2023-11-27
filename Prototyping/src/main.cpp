d9c188b27315bae1e8406725dfc8946e9a8151b5
alan <acard6@illinois.edu>
2023-11-27 15:33:20 -0600

    powerboard output setup

--- Prototyping/src/main.cpp
+++ Prototyping/src/main.cpp
@@ -5,167 +5,212 @@
 #include <cstdint>
 #include <driver/uart.h>
 #include <string.h>
-#include <time.h>
-
-  #define ORANGE 1
-  #define GREEN 2
-  #define YELLOW 42
-  #define WHITE 41
-  #define BLUE 48
-
-  #define BUTTON 4 
-  #define DHTPIN 40
-  #define DHTTYPE DHT22
-
-  #define TEMP_LED 47
-
-  #define MIN_INTERVAL  2000
-  #define TIMEOUT UINT32_MAX
-
-  #define BUF_SIZE 1024
-  // put function declarations here:
-  // int myFunction(int, int);
-  
+// #include <time.h>
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
 
 
+#define BAUD_RATE 115200
 
-  void butt_count();
-  void hum_to_led(uint8_t h);
+// #define RELAY 6
+// #define BUTTON 4 
+#define DHTTYPE DHT22
 
-  int counter = 0x0;
-  DHT dht(DHTPIN, DHTTYPE);
+//deinfing GPIO pins and their use
+#define SO      4     /* data input from MAX31855 temperature sensors   : input  */
+#define SCK     5     /* source clock for MAX31855 temp sensors         : output */
+#define A       6     /* bit selector for MUX                           : output */
+#define B       7     /* bit selector for MUX                           : output */
+#define C       15    /* bit selector for MUX                           : output */
+#define G2A     16    /* enable on MUX                                  : output */
+#define G2B     17    /* enable on MUX                                  : output */
+#define G1      18    /* enable on MUX                                  : output */
+#define CS9     8     /* CS for 9 temp sensor not accessable by MUX     : output */
 
-  void setup() {
-    // put your setup code here, to run once:
-    // int result = myFunction(2, 3);
-    pinMode(ORANGE, OUTPUT);
-    pinMode(GREEN,  OUTPUT);
-    pinMode(YELLOW, OUTPUT);
-    pinMode(WHITE,  OUTPUT);
-    pinMode(BLUE,   OUTPUT);
+#define DHTPIN1 19    /* humidity sensor 1 data bus                     : input  */
+#define DHTPIN2 20    /* humidity sensor 2 data bus                     : input  */
 
-    pinMode(TEMP_LED, OUTPUT);
-    pinMode(BUTTON, INPUT);
-    Serial.begin(9600);
-    Serial.println("DHTxx test!");
+#define SUB1    10    /* pin to power board                             : I/O    */
+#define SUB2    11    /* pin to power board                             : I/O    */
+#define SUB3    12    /* pin to power board                             : I/O    */
+#define SUB4    13    /* pin to power board                             : I/O    */
+#define SUB5    14    /* pin to power board                             : I/O    */
+#define SUB6    21    /* pin to power board                             : I/O    */
+#define SUB7    47    /* pin to power board                             : I/O    */
+#define SUB8    48    /* pin to power board                             : I/O    */
+#define dummy   42    /* dummy pin for CS in temp sensors with MUX      : output */
 
-    dht.begin();
-  }
+DHT dht1(DHTPIN1, DHTTYPE);
+DHT dht2(DHTPIN2, DHTTYPE);
 
-  void loop() {
-    // void butt_count();
-    delay(500);
-    time_t start = time(NULL);
-    float H = dht.readHumidity();
-    time_t end = time(NULL);
-    float t = dht.readTemperature();
-    float f = dht.readTemperature(true);
+Adafruit_MAX31855 temp9(SCK, CS9, SO);
+Adafruit_MAX31855 temp7(SCK, dummy, SO);
+// Adafruit_MAX31855 temp6(SCK, dummy, SO);
+// Adafruit_MAX31855 temp5(SCK, dummy, SO);
+// Adafruit_MAX31855 temp4(SCK, dummy, SO);
+// Adafruit_MAX31855 temp3(SCK, dummy, SO);
+// Adafruit_MAX31855 temp2(SCK, dummy, SO);
+// Adafruit_MAX31855 temp1(SCK, dummy, SO);
+// Adafruit_MAX31855 temp0(SCK, dummy, SO);
+//  _________         _________ 
+//  | 13  14 |        |  4   5 |
+//  | 12  21 |        |  3   6 |
+//  | 11  47 |  --->  |  2   7 |
+//  | 10  48 |        |  1   8 |
+//  | 5V  5V |        | 5V  5V |
+//  |_______ |        |_______ |
+int arr[8] = {SUB1, SUB2, SUB3, SUB4, SUB5, SUB6, SUB7, SUB8};
+void setup() {
+  Serial.begin(BAUD_RATE);
+  /* -----set pins to I/O----- */
+  pinMode(SO, INPUT);      // input         data bus for temp sensors
+  pinMode(SCK, OUTPUT);    // output        clock bus for temp sensors
+  pinMode(CS9, OUTPUT);    // output        chip select for temp sensor not connected by mux
+  pinMode(A, OUTPUT);      // output        3:8 mux chip select for temp sensors
+  pinMode(B, OUTPUT);      // output        3:8 mux chip select for temp sensors
+  pinMode(C, OUTPUT);      // output        3:8 mux chip select for temp sensors
+  pinMode(G1, OUTPUT);     // output        3:8 mux enables for temp sensors
+  pinMode(G2B, OUTPUT);    // output        3:8 mux enables for temp sensors
+  pinMode(G2A, OUTPUT);    // output        3:8 mux enables for temp sensors
 
-    Serial.println(end - start);
-
-    while (Serial.available() == 0) {
-    }
-
-    int read = Serial.parseInt();
-    if (read == 1){
-
-      Serial.print("humidity is(%):");
-      Serial.print(H);
-      Serial.print(" %\t");
-      Serial.print("temperatire is(%):");
-      Serial.print(t);
-      Serial.print(" *C\t");
-      Serial.print(f);
-      Serial.print(" *F\n");    
-    
-      H /= 3;
-      uint8_t h = (uint8_t)H;
-      hum_to_led(h);
-      
-      digitalWrite(TEMP_LED, HIGH);
-
-      delay(10000);
-    }
-    else{
-      digitalWrite(TEMP_LED, LOW);
-      digitalWrite(BLUE, LOW);
-      digitalWrite(WHITE, LOW);
-      digitalWrite(GREEN, LOW);
-      digitalWrite(ORANGE, LOW);
-      digitalWrite(YELLOW, LOW);
-      
-    }
-  }
-
-// put function definitions here:
-/*
-int myFunction(int x, int y) {
-  return x + y;
+  pinMode(DHTPIN1, INPUT); // input         humidity sensors
+  pinMode(DHTPIN2, INPUT); // input         humidity sensors
+  pinMode(SUB1, OUTPUT);   // input/output  pinhears on main board
+  pinMode(SUB2, OUTPUT);   // input/output  pinhears on main board
+  pinMode(SUB3, OUTPUT);   // input/output  pinhears on main board
+  pinMode(SUB4, OUTPUT);   // input/output  pinhears on main board
+  pinMode(SUB5, OUTPUT);   // input/output  pinhears on main board
+  pinMode(SUB6, OUTPUT);   // input/output  pinhears on main board
+  pinMode(SUB7, OUTPUT);   // input/output  pinhears on main board
+  pinMode(SUB8, OUTPUT);   // input/output  pinhears on main board
+  /* -----initialize out pins----- */
+  digitalWrite(G1, HIGH);   // based on sn74hc138 data sheet so that no sensors has cs enabled
+  digitalWrite(G2B, LOW);   // based on sn74hc138 data sheet so that no sensors has cs enabled
+  digitalWrite(G2A, LOW);   // based on sn74hc138 data sheet so that no sensors has cs enableds
+  Serial.println("Flashed & Running.");
+  dht1.begin();
+  // dht2.begin();
+  temp9.begin();    // U13 sensor
+  temp7.begin();    // U12 sensor
+//  temp6.begin();    // U11 sensor
+//  temp5.begin();    // U10 sensor
+//  temp4.begin();    // U9 sensor
+//  temp3.begin();    // U8 sensor
+//  temp2.begin();    // U7 sensor
+//  temp1.begin();    // U6 sensor
+//  temp0.begin();    // U5 sensor
 }
-*/
-    void hum_to_led(uint8_t h){
-      if ((h&1) == 1)
-        digitalWrite(ORANGE, HIGH);
-      else
-        digitalWrite(ORANGE, LOW);
-    
-      if (((h>>1)&1) == 1)
-        digitalWrite(GREEN, HIGH);
-      else
-        digitalWrite(GREEN, LOW);
 
-      if (((h>>2)&1) == 1)
-        digitalWrite(YELLOW, HIGH);
-      else
-        digitalWrite(YELLOW, LOW);
+void loop() {
 
-      if (((h>>3)&1) == 1)
-        digitalWrite(WHITE, HIGH);
-      else
-        digitalWrite(WHITE, LOW);
+  int read = Serial.parseInt();
 
-      if (((h>>4)&1) == 1)
-        digitalWrite(BLUE, HIGH);
-      else
-        digitalWrite(BLUE, LOW);
-    }
-
-  void butt_count(){
-    if (digitalRead(BUTTON) == HIGH){
-      counter++;
-      Serial.println(counter);
-    }
-
-    if ((counter % 2)){
-      digitalWrite(ORANGE, HIGH);
-    }else{
-      digitalWrite(ORANGE, LOW);
-    }
-
-
-    if (((counter >> 1) % 2)){
-      digitalWrite(GREEN, HIGH);
-    }else{
-      digitalWrite(GREEN, LOW);
-    }
-
-    if (((counter >> 2) % 2)){
-      digitalWrite(YELLOW, HIGH);
-    }else{
-      digitalWrite(YELLOW, LOW);
-    }
-
-    if (((counter >> 3) % 2)){
-      digitalWrite(WHITE, HIGH);
-    }else{
-      digitalWrite(WHITE, LOW);
-    }
-
-    if (((counter >> 4) % 2)){
-      digitalWrite(BLUE, HIGH);
-    }else{
-      digitalWrite(BLUE, LOW);
-    }
-
-    if (counter >= 32){counter = 0;}
+  /* get humidity sensor data */
+  if (read == 8){
+    getHumData();
   }
+
+  /* get lone thermocouple */
+  if (read == 9){
+    double c = temp9.readCelsius();       // temp sensor U13 temp reading
+    Serial.print("temp sensor:\t temp:");
+    Serial.print(c);
+    Serial.println(" *C");
+  }
+  /* get thermocouples in MUX data */
+  if (10 <= read && read <18){
+    uint8_t i = read % 10;
+    float temp_c;
+    setMuxChannel(i);
+    if (i == 0){
+      // temp_c = temp0.readCelsius();
+    }
+    if (i == 1){
+      //temp_c = temp1.readCelsius();
+    }
+    if (i == 2){
+      //temp_c = temp2.readCelsius();
+    }
+    if (i == 3){
+      //temp_c = temp3.readCelsius();
+    }
+    if (i == 4){
+      //temp_c = temp4.readCelsius();
+    }
+    if (i == 5){
+      //temp_c = temp5.readCelsius();
+    }
+    if (i == 6){
+      //temp_c = temp6.readCelsius();
+    }
+    if (i == 7){
+      //temp_c = temp7.readCelsius();
+    }
+    digitalWrite(G2B, HIGH);
+    printTemp(temp_c, i);
+  }
+
+
+  /* set pins to power */
+  if (30 <= read && read < 50){
+    powerBoard(read);
+  }
+
+}
+
+void getHumData(){
+  float H = dht1.readHumidity();        // humidity sensor J10 humidity reading
+  float t = dht1.readTemperature();     // humidity sensot J10 temp reading in *C
+ 
+  Serial.print("humidity:");
+  Serial.print(H);
+  Serial.print(" %\t temp:");
+  Serial.print(t);
+  Serial.println(" *C");
+  
+}
+
+
+/* Set pins for the power subsystem board (note: works) */
+void powerBoard(int read){
+  int i, port, val;
+  if (read >= 30){
+    if (read < 40){
+      i = read % 10;
+      port = arr[i];
+      val = HIGH;
+    }
+  }
+  if (read >= 40){
+    if (read < 50){
+      i = read % 10;
+      port = arr[i];
+      val = LOW;
+    }
+  }
+  digitalWrite(port, val);
+  Serial.print("port: ");
+  Serial.print(port);
+  Serial.print(" is ");
+  Serial.println(val);
+}
+
+/* Set MUX to select the desired channel */
+void setMuxChannel(int channel) {
+  digitalWrite(G2B, LOW);
+  digitalWrite(A, (channel & 1));
+  digitalWrite(B, ((channel >> 1) & 1));
+  digitalWrite(C, ((channel >> 2) & 1));
+}
+
+/* Print temperature values from MUX thermos  */
+void printTemp(int temp_c, int i){
+  Serial.println("temp reading with MUX:");
+  Serial.print("Thermoucouple ");
+  Serial.print(i);
+  Serial.print(" temp: ");
+  Serial.print(temp_c);
+  Serial.println(" *C");
+  delay(10);
+}
+
